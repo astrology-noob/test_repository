ab39fafa0b982a06d08d7b2e1505c4d3441ea0ce
Melanie <melanie@astra>
2022-05-14 22:40:07 +0300

    script.sh

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir folder1
+mkdir folder2
+mkdir folder3
+mkdir folder4
+mkdir folder5
+mkdir folder6
+mkdir folder7
+mkdir folder8
+mkdir folder9
+mkdir folder10
+echo 'Скрипт завершился успешно!'
